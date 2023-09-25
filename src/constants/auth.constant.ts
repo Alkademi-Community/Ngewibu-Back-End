67d1e97f71dbc919658d4863d2317aea713372d3
Strayneko <bayurendi10@gmail.com>
2023-09-25 14:30:22 +0700

    (+) Constants

---
+++ src/constants/auth.constant.ts
@@ -0,0 +1,11 @@
+const AuthMessage = {
+  USER_NOT_FOUND:
+    "Sorry, we couldn't find that user. Please check your username or email and try again, or sign up if you're new.",
+  WRONG_PASSWORD:
+    "Oops! It seems you've entered an incorrect password. Please double-check your password and try again.",
+  MISSING_AUTH_TOKEN:
+    'Uh-oh! It looks like you forgot to include an authentication token. To continue, please provide a valid authentication token.',
+  TOKEN_INVALID:
+    'Oops! The authentication token you provided is invalid. Please ensure you have the correct token and try again.',
+}
+export default AuthMessage
