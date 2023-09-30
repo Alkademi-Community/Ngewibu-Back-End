15185cd7ef0f7ac5bdcf0c31f31be947935cbfb0
Strayneko <bayurendi10@gmail.com>
2023-10-01 01:22:50 +0700

    (~) Update controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,36 +1,76 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Req,
+  Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common'
 import { UserService } from './user.service'
-import { Request } from 'express'
 import { IsAuthenticated } from 'src/guard/is-authenticated.guard'
-import { User } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 import { ResponseData } from 'src/types/response'
 import * as httpStatus from 'http-status'
 import AuthMessage from 'src/constants/auth.constant'
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { SessionService } from 'src/service/session.service'
+import { SessionData } from 'src/types/session'
+import { UserProfileService } from './user-profile.service'
+import { Request, Response } from 'express'
+import { UpdateUserProfile } from 'src/validation/update-user-profile.dts'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
+import { StorageConstants } from 'src/constants/file.constant'
+
+import {
+  imageFilter,
+  getUniqueFilename,
+} from 'src/utils/image-upload-handler.util'
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private sessionService: SessionService,
+    private userProfileService: UserProfileService,
+  ) {}
 
   /**
-   * Get one specific user record by username
-   * @param {any} 'profile/:username'
+   * get the data of the user who is currently logged in
+   * @param {any} 'profile
    * @returns {any}
    */
 
-  @Get('profile/:username')
+  @Get('profile')
   @ApiBearerAuth('Authorization')
-  @ApiParam({
-    name: 'username',
-    example: 'johndoe123',
-  })
   @UseGuards(IsAuthenticated)
-  public async profile(@Req() request: Request) {
-    const user: User | undefined | null = await this.userService.findOne(
-      request?.params?.username,
-    )
+  public async profile(
+    @Headers() headers: Record<string, string>,
+    @Res() res: Response,
+  ) {
+    const authToken = this.sessionService.getAuthToken(headers)
+    const sessionData: SessionData =
+      await this.sessionService.getSessionData(authToken)
     let response: ResponseData
+
+    const sessionDataIsNull: boolean = sessionData === null
+    if (sessionDataIsNull) {
+      response = {
+        status: false,
+        status_code: httpStatus.UNAUTHORIZED,
+        message: AuthMessage.TOKEN_INVALID,
+      }
+      return response
+    }
+
+    const user: User | undefined | null = await this.userService.findOne(
+      sessionData?.username,
+    )
 
     const userIsNotFound = [null, undefined].includes(user)
     if (userIsNotFound) {
@@ -48,6 +88,65 @@ export class UserController {
       message: '',
       data: user,
     }
-    return response
+    return res.status(response.status_code).json(response)
+  }
+
+  @Post('profile/update')
+  @ApiBearerAuth('Authorization')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      fileFilter: imageFilter,
+      limits: { fileSize: StorageConstants.oneMb * 4 },
+      storage: diskStorage({
+        destination: StorageConstants.userProfileImagePath,
+        filename: getUniqueFilename,
+      }),
+    }),
+  )
+  @UseGuards(IsAuthenticated)
+  public async updateProfile(
+    @Headers() headers: Record<string, string>,
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() body: UpdateUserProfile,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    const authToken = this.sessionService.getAuthToken(headers)
+    const sessionData: SessionData =
+      await this.sessionService.getSessionData(authToken)
+    let response: ResponseData
+
+    const sessionDataIsNull: boolean = sessionData === null
+    if (sessionDataIsNull) {
+      response = {
+        status: false,
+        status_code: httpStatus.UNAUTHORIZED,
+        message: AuthMessage.TOKEN_INVALID,
+      }
+      return response
+    }
+
+    const { gender_id, name, bio, date_of_birth, address } = body
+    const data: Prisma.UserProfileUncheckedUpdateInput = {
+      genderId: Number(gender_id),
+      name,
+      bio,
+      dateOfBirth: date_of_birth,
+      address,
+    }
+
+    const userUploadImage = image !== undefined
+    const schemeAndHost = `${req.protocol}://${req.headers.host}`
+    if (userUploadImage) {
+      data.imageUrl = `${schemeAndHost}/${image.path}`
+    }
+
+    const update = await this.userProfileService.updateProfile(
+      sessionData.id,
+      data,
+      schemeAndHost,
+    )
+
+    return res.status(update?.status_code).json(update)
   }
 }
